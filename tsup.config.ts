b9b1ceaa51e819c133260d96766b6ee9bf6af4db
goenning <me@goenning.net>
2023-10-27 20:13:33 +0100

    move to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+const { version } = require("./package.json");
+
+export default defineConfig({
+  entry: ["src/index.ts", "src/main.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  minify: true,
+  sourcemap: true,
+  clean: true,
+  env: {
+    PKG_VERSION: version,
+  },
+  outExtension({ format }) {
+    return {
+      js: `.${format === "esm" ? "mjs" : "cjs"}`,
+    };
+  },
+});
